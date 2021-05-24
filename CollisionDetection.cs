12bbcd7926e1db06a2d4c383f99daf37d5f60ad5
delchevm <59831870+delchevm@users.noreply.github.com>
2021-05-24 22:14:40 +0100

    Update CollisionDetection.cs

--- CollisionDetection.cs
+++ CollisionDetection.cs
@@ -2,25 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Amends score if a player fails to hit a target based on target type and destroys the game object
 public class CollisionDetection : MonoBehaviour
 {
 
     private int score;
-
-
+    
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Enemy")
-        {
-            score = Score.EnemiesDestroyed;
-            if (score > 0)
-            {
-                Score.EnemiesDestroyed--;
-            }
-            Destroy(collision.gameObject);
-
-        }
-        else if (collision.tag == "UFO")
+        if (collision.tag == "Enemy" || collision.tag == "UFO")
         {
             score = Score.EnemiesDestroyed;
             if (score > 0)
